b149065a6eb99e46367477e6b68730d0b11ad500
manho <86037446+manho30@users.noreply.github.com>
2022-03-02 13:22:04 +0800

    Update play.js

--- js/play.js
+++ js/play.js
@@ -1,3 +1,11 @@
 document.getElementsByClassName("about").addEventListener("click", () => {
     document.getElementById("music").play();
 })
+
+document.getElementsByClassName("Intro_text").addEventListener("click", () => {
+    document.getElementById("music").play();
+})
+
+document.getElementsByClassName("proj").addEventListener("click", () => {
+    document.getElementById("music").play();
+})
